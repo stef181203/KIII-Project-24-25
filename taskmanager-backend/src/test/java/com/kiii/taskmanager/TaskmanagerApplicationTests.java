15e32515392b91b586829fe9a19f908095db01cc
stef181203 <petrovskistefan90@gmail.com>
2025-06-22 15:37:08 +0200

    Setting up the backend project

---
+++ taskmanager-backend/src/test/java/com/kiii/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiii.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
