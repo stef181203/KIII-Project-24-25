64b5c772d2dccdea7ef50f636c00e253a808c49a
stef181203 <petrovskistefan90@gmail.com>
2025-06-28 18:58:27 +0200

    Updated docker orchestration #4

--- taskmanager-frontend/vite.config.js
+++ taskmanager-frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-if (!window.SERVER_PORT) {
-  console.warn("window.SERVER_PORT is not defined. Falling back to default.");
-}
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: window.SERVER_PORT || 3000,
+    port: 3000,
   },
 })
